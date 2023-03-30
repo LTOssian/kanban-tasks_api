8ffcf4d512ff1d014ec54540856464d6966ddaac
Louisan TCHITOULA <t.louisan@outlook.fr>
2023-03-30 14:59:05 +0200

    feat: id get and post requests

--- server/controllers/boards.controller.ts
+++ server/controllers/boards.controller.ts
@@ -5,13 +5,44 @@ const promisePool = pool.promise();
 
 export const boardsController = {
     getAll: async (req: Request, res: Response) => {
-        try {
-            const [rows, fields] = await promisePool.query("SELECT * FROM boards");
+        const [rows, _] = await promisePool.query(
+            `SELECT * FROM boards`
+        );
+
+        if (rows.length) {
             res.json({
                 data: rows
             })
-        } catch(err) {
-            console.log(err)
+        } else {
+            res.status(404).json({
+                state: "error"
+            })
         }
     },
+    getById: async (req: Request, res: Response) => {
+
+        const { id } = req.params
+        const [rows, _] = await promisePool.query(
+            `SELECT * FROM boards WHERE id = ${id}`
+        );
+
+        if (rows.length) {
+            res.json({
+                data: rows
+            })
+        } else {
+            res.status(404).json({
+                state: "error"
+            })
+        }
+    },
+    postBoard: async (req: Request, res: Response) => {
+        const { name } = req.body
+        const sqlQuery = `INSERT INTO boards (name) values (${name})`
+
+        const [rows, _] = await promisePool.query(sqlQuery);
+        res.json({
+            data: rows
+        })
+    }
 }
