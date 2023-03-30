a8e70b6e37223bf7aa076c75032f6c56c18cea4d
Louisan TCHITOULA <t.louisan@outlook.fr>
2023-03-30 15:53:06 +0200

    feat: delete request for boards

--- server/controllers/boards.controller.ts
+++ server/controllers/boards.controller.ts
@@ -12,16 +12,16 @@ export const boardsController = {
         if (rows.length) {
             res.json({
                 data: rows
-            })
+            });
         } else {
             res.status(404).json({
                 state: "error"
-            })
+            });
         }
     },
     getById: async (req: Request, res: Response) => {
 
-        const { id } = req.params
+        const { id } = req.params;
         const [row, _] = await promisePool.query(
             `SELECT * FROM boards WHERE id = ${id}`
         );
@@ -29,42 +29,51 @@ export const boardsController = {
         if (row.length) {
             res.json({
                 data: row
-            })
+            });
         } else {
             res.status(404).json({
                 state: "error"
-            })
+            });
         }
     },
     postBoard: async (req: Request, res: Response) => {
-        const { name } = req.query
+        const { name } = req.query;
         if (name) {
             const [newRow, _] = await promisePool.query(
                 `INSERT INTO boards (id, name) VALUES (null,"${name}")`
             );    
             res.status(201).json({
                 data: newRow
-            })
+            });
         } else {
             res.status(404).json({
                 state: "error"
-            })
+            });
         }
     },
     updateBoard: async (req: Request, res: Response) => {
-        const { name } = req.query 
-        const { id } = req.params
+        const { name } = req.query ;
+        const { id } = req.params;
         if (name) {
             const [updatedRow, _] = await promisePool.query(
                 `UPDATE boards SET name ="${name}" WHERE id = ${id}`
-            )
+            );
             res.json({
                 data: updatedRow
-            })
+            });
         } else {
             res.status(404).json({
                 state: "error"
-            })
+            });
         }
+    },
+    deleteBoard: async (req: Request, res: Response) => {
+        const { id } = req.params;
+        const [deleteRow, _] = await promisePool.query(
+            `DELETE FROM boards WHERE id=${id}`
+        );
+        res.status(204).json({
+            data: deleteRow
+        })
     }
 }
--- server/routes/boardsRouter.ts
+++ server/routes/boardsRouter.ts
@@ -1,8 +1,14 @@
-import express, { Router, Request, Response } from "express";
+import express, { Router, Request, Response, NextFunction } from "express";
 import { boardsController } from "../controllers/boards.controller";
 
 export const boardsRouter: Router = express.Router();
+
 boardsRouter.get("/", boardsController.getAll);
+
 boardsRouter.get("/:id", boardsController.getById);
+
 boardsRouter.post("/", boardsController.postBoard);
+
 boardsRouter.put("/:id", boardsController.updateBoard);
+
+boardsRouter.delete("/:id", boardsController.deleteBoard)
