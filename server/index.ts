e88eadd9c10bfbc2d4f9af3c144f1033390e1514
Louisan TCHITOULA <t.louisan@outlook.fr>
2023-03-30 20:33:15 +0200

    feat: import morgan's dev config

--- server/index.ts
+++ server/index.ts
@@ -3,15 +3,19 @@ import * as dotenv from "dotenv";
 import cors from 'cors';
 import { boardsRouter } from './routes/boardsRouter';
 import { columnsRouter } from './routes/columnsRouter';
+import morgan from 'morgan';
 
 dotenv.config()
 
 const app: Express = express();
 
 app.use(cors());
+app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 app.use('/api/boards', boardsRouter);
 const PORT:string | number = process.env.PORT || 3000;
-app.listen(PORT);
+app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+});
