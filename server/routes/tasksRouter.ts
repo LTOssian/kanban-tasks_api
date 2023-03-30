2fc757c481ea883bcb9a21eeaba5075bac0e9e5c
Louisan TCHITOULA <t.louisan@outlook.fr>
2023-03-30 21:57:31 +0200

    feat: post middleware

--- server/routes/tasksRouter.ts
+++ server/routes/tasksRouter.ts
@@ -1,3 +1,8 @@
 import express, { Router } from "express";
+import { tasksController } from "../controllers/tasks.controller";
 
-export const tasksRouter = express.Router();
+export const tasksRouter: Router = express.Router();
+
+tasksRouter.get("/", tasksController.getAllByColumn);
+tasksRouter.get("/:id", tasksController.getById);
+tasksRouter.post("/", tasksController.postTask);
