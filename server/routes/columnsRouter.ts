6c40d680e75b8b9ca0619daf9b7d221d9db57c04
Louisan TCHITOULA <t.louisan@outlook.fr>
2023-03-30 18:30:55 +0200

    feat: add middleware for delete request

--- server/routes/columnsRouter.ts
+++ server/routes/columnsRouter.ts
@@ -4,5 +4,11 @@ import { columnsController } from "../controllers/columns.controller";
 export const columnsRouter: Router = express.Router();
 
 columnsRouter.get("/", columnsController.getAll);
+
 columnsRouter.get("/:id", columnsController.getById);
+
 columnsRouter.post("/", columnsController.postColumn);
+
+columnsRouter.put("/:id", columnsController.updateColumn);
+
+columnsRouter.delete("/:id", columnsController.deleteColumn);
