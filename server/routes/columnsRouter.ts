9a83e0e676c65cb8f19ba27d76592fc1904618ee
Louisan TCHITOULA <t.louisan@outlook.fr>
2023-03-30 20:41:25 +0200

    feat: delete request middleware & merge params for tasks

--- server/routes/columnsRouter.ts
+++ server/routes/columnsRouter.ts
@@ -1,5 +1,7 @@
-import express, { Router } from "express";
+import express, { NextFunction, Router, Response } from "express";
 import { columnsController } from "../controllers/columns.controller";
+import { columnIdRequest } from "../interfaces/interfaces";
+import { tasksRouter } from "./tasksRouter";
 
 export const columnsRouter: Router = express.Router();
 
@@ -12,3 +14,9 @@ columnsRouter.post("/", columnsController.postColumn);
 columnsRouter.put("/:id", columnsController.updateColumn);
 
 columnsRouter.delete("/:id", columnsController.deleteColumn);
+
+columnsRouter.use('/:columnId/tasks', tasksRouter);
+columnsRouter.param('columnId', (req: columnIdRequest, res: Response, next: NextFunction, columnId) => {
+    req.columnId = columnId;
+    next();
+})
