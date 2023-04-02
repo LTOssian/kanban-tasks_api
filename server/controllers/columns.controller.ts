a66a34ea780f6f33ad03f40235c78264838e152b
Louisan TCHITOULA <t.louisan@outlook.fr>
2023-04-02 22:07:21 +0200

    fix: separate sql logic from controller for columns

--- server/controllers/columns.controller.ts
+++ server/controllers/columns.controller.ts
@@ -1,75 +1,90 @@
 import { Request, Response } from "express";
-import { promisePool } from "../database";
 import { RequestSuperSet } from "../interfaces/interfaces";
+import { columnModel } from "../models/columns.model";
 
 export const columnsController = {
     getAllByBoard: async (req: RequestSuperSet, res: Response) => {
-        const [rows] = await promisePool.query(
-            `SELECT c.name, c.id, c.board_id, COUNT(t.id) as tasks_quantity FROM columns c LEFT JOIN tasks t ON c.id = t.column_id WHERE c.board_id = ${req.boardId} GROUP BY c.id;`
-        );
-        if (rows.length) {
-            res.json({
-                data: rows
-            }) 
-        } else {
+        try {
+            const rows = await columnModel.getAllByBoardFromDB(Number(req.boardId));
+            if (rows.length) {
+                res.json({
+                    data: rows
+                }) 
+            } else {
+                res.status(404).json({
+                    state: "error"
+                });
+            } 
+        } catch(err) {
             res.status(404).json({
-                state: "error"
+                state: "error",
+                error: err
             })
         }
     },
     getById: async (req: RequestSuperSet, res: Response) => {
         const { id } = req.params;
-        const [row] = await promisePool.query(
-            ` select c.*, count(t.id) as number_of_tasks from columns c join tasks t on c.id = t.column_id WHERE c.id = ${id} AND c.board_id = ${req.boardId}`
-        );
-        if (id == row[0]["id"]) {
-            res.json({
-                data: row
-            })    
-        } else {
+        try {
+            const row = await columnModel.getByIdFromDB(parseInt(id, 10), Number(req.boardId))
+            if (row.id) {
+                res.json({
+                    data: row
+                });
+            } else {
+                res.status(404).json({
+                    state: "error"
+                });
+            } 
+        } catch(err) {
             res.status(404).json({
-                state: "error"
-            });
+                state: "error",
+                error: err
+            })
         }
     },
     postColumn: async (req: RequestSuperSet, res: Response) => {
-        const { name } = req.query;
-        if (name) {
-            const [newRow] = await promisePool.query(
-                `INSERT INTO columns (id, name, board_id) VALUES (NULL, '${name}', ${req.boardId});`
-            );
-            res.status(201).json({
-                data: newRow
-            });
-        } else {
+        const name = req.query. name as string;
+        try {
+            if (name) {
+                await columnModel.postColumnToDB(name, Number(req.boardId))
+                res.status(201).json();
+            }
+        } catch(err) {
             res.status(404).json({
-                state: "error"
-            });
+                state: "error",
+                error: err
+            })
         }
     },
     updateColumn: async(req: RequestSuperSet, res: Response) => {
-        const { name } = req.query;
+        const name = req.query. name as string;
         const { id } = req.params;
-        if ( name ) {
-            const [updateRow] = await promisePool.query(
-                `UPDATE columns SET name="${name}" WHERE id = ${id} AND board_id = ${req.boardId}`
-            );
-            res.json({
-                data: updateRow
-            });
-        } else {
+        try {
+            if ( name ) {
+                await columnModel.updateColumnOnDB(parseInt(id,10), name);
+                res.json();
+            } else {
+                res.status(404).json({
+                    state: "error"
+                });
+            }    
+        } catch(err) {
             res.status(404).json({
-                state: "error"
-            });
+                state: "error",
+                error: err
+            })
         }
     },
     deleteColumn: async (req: Request, res: Response) => {
         const { id } = req.params;
-        const [deletedRow]= await promisePool.query(
-            `DELETE FROM columns WHERE id=${id}`
-        );
-        res.status(204).json({
-            data:deletedRow
-        })
+        try {
+            await columnModel.deleteColumnFromDB(parseInt(id));
+            res.status(204).json()
+        } catch(err) {
+            res.status(404).json({
+                state: "error",
+                error: err
+            })
+        }
     }
 }
