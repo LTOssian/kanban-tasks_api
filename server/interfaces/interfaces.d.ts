bc7ce521f5d18375aca485d884b2d51b5b99de8a
Louisan TCHITOULA <t.louisan@outlook.fr>
2023-04-02 17:39:29 +0200

    feat: board interface

--- server/interfaces/interfaces.d.ts
+++ server/interfaces/interfaces.d.ts
@@ -1,7 +1,14 @@
 import { Request } from "express";
+import { Generated, MysqlDialect } from "kysely";
 
 export interface RequestSuperSet extends Request {
     boardId ?: string;
     columnId ?: string;
     taskId ?: string
 }
+
+export interface BoardTable {
+    id: Generated<number>
+    name: string
+}
+
