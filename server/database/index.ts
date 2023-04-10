b0c560035bcc1b1ac81a881a5cdb7f6220b49e5b
Louisan TCHITOULA <t.louisan@outlook.fr>
2023-04-10 13:34:41 +0200

    fix: rm unused mysql2 pool

--- server/database/index.ts
+++ server/database/index.ts
@@ -5,14 +5,6 @@ import { Kysely, MysqlDialect  } from 'kysely'
 
 dotenv.config();
 
-
-export const promisePool = createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-}).promise();
-
 export const db = new Kysely<Database>({
     dialect: new MysqlDialect({
         pool: createPool({
